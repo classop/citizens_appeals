3b4081a6c696027521021f75715f5fd910ca1ba2
classop <gptlocfy@gmail.com>
2026-05-19 12:13:34 +0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,6 @@
 from app import create_app
 
-# Создаем экземпляр приложения один раз при импорте
-app = create_app()
+application = create_app()
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    application.run()
